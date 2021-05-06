6cf5de7459cadb1374a21463efb3e5497d5a5fbc
nguyenduccanh120394 <duccanhh94@gmail.com>
2021-05-06 14:41:18 +0700

    Demo

---
+++ src/Demo_SetupMenu.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Demo_SetupMenu{
+    public static void main(String[] args) {
+        System.out.println("Menu");
+        System.out.println("1. Draw the triangle");
+        System.out.println("2. Draw the square");
+        System.out.println("3. Draw the rectangle");
+        System.out.println("0. Exit");
+        System.out.println("Enter your choice");
+        Scanner input = new Scanner(System.in);
+        int choice = input.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Draw the triangle");
+                System.out.println("*******");
+                System.out.println("******");
+                System.out.println("*****");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("*");
+                break;
+            case 2:
+                System.out.println("Draw the square");
+                System.out.println("**********");
+                System.out.println("**********");
+                System.out.println("**********");
+                System.out.println("**********");
+                System.out.println("**********");
+                System.out.println("**********");
+                break;
+            case 3:
+                System.out.println("Draw the rectangle");
+                System.out.println("*****************");
+                System.out.println("*****************");
+                System.out.println("*****************");
+                System.out.println("*****************");
+                System.out.println("*****************");
+                break;
+        }
+        }
+    }
